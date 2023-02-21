94ab6409f7b58e9768ed382033caabc8b3bb3acd
stan-lior1999 <stanleyogodi806@gmail.com>
2023-02-21 12:57:11 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lower case alphabet
+ */
+void print_alphabet(void);
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z';  letter++)
+		_putchar(letter);
+	_putchar("\n");
+
+}
