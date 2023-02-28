f5a24654f88fc02ea897f0124f937abe42b324a3
stan-lior1999 <stanleyogodi806@gmail.com>
2023-02-27 18:31:21 -0800

    spontrr

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * Owned By stanley 
+ * Owned By stanley
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
