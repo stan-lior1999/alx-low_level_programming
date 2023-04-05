a3bb1d5f31be12de0cb727327400d8e9c242e5eb
stan-lior1999 <stanleyogodi806@gmail.com>
2023-04-05 15:04:55 -0700

    0 task in more linkedlist

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+thes task is for the more linked list i am going to
+learn alot from these task
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+
+/**
+ * struct listint_s-singly linked list
+ * @n:integer
+ * @next:point to the next node
+ * Discription: singly linked list node struction
+ *
+ */
+	typedef struct listint_s
+{
+
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/*function prototypes */
+size_t print_listint(const listint_t *h);
+#endif
